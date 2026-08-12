5f7a18d8e087b127187abce4195769f98efaec97
beatrizkodayashi <beatriz.taveira04@gmail.com>
2026-08-12 02:38:13 -0300

    fix: aumenta mascote do empty state de insights

--- src/js/pages/insights.js
+++ src/js/pages/insights.js
@@ -98,7 +98,7 @@ export async function renderInsights(container) {
       `) : renderCard('', `
         <div class="empty-state py-4">
           <div class="duck-companion">
-            <img src="/pato_padrao.png" alt="${APP_NAME}" class="bloom-mascot-img bloom-mascot-img--empty" width="120" height="120" decoding="async" />
+            <img src="/pato_padrao.png" alt="${APP_NAME}" class="bloom-mascot-img bloom-mascot-img--empty" width="200" height="200" decoding="async" />
             <p class="mascot-caption">Registre mais ciclos para ver insights detalhados.</p>
           </div>
         </div>
